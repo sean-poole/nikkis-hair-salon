51f2cc0f1d5a937a897c92a7a91ca2521c64d682
Sean Poole <spoole5289@gmail.com>
2024-09-09 13:36:22 -0700

    hosting configuration changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "./config/.env" });
+// require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config();
 
 const bookingRoutes = require("./routes/bookings");
 
